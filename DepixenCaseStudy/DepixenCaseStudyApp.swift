91858e24dfa3472e3c0c855058170040af969d1d
Berkay-Disli <berkaydisli.work@gmail.com>
2023-03-03 19:11:03 +0300

    Initial Commit

---
+++ DepixenCaseStudy/DepixenCaseStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DepixenCaseStudyApp.swift
+//  DepixenCaseStudy
+//
+//  Created by Berkay Disli on 3.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct DepixenCaseStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
